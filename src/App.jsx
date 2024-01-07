f0557a816162131333b2c3a30f410ddf293a9dd4
mcardenasag <a.n.yelo_18@hotmail.com>
2024-01-07 12:05:57 -0500

    Modificacion y eliminacion de  archivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0
+  })
+
+  // const handleLeftClick = () => {
+  //   const newClicks = {
+  //     ...clicks,
+  //     left: clicks.left + 1,
+      
+  //   }
+  //   console.log(clicks)
+  //   setClicks(newClicks)
+  // }
+
+  // simplificando la funcion 
+
+  const handleLeftClick = () =>
+    setClicks({...clicks, left: clicks.left +1})
+
+  // const handleRightClick = () => {
+  //   const newClicks = {
+  //     ...clicks,
+  //     right: clicks.right + 1
+  //   }
+  //   console.log(clicks)
+  //   setClicks(newClicks)
+  // }
+
+  // simplificando la funcion 
+
+  const handleRightClick = () =>
+  setClicks({...clicks, right: clicks.right +1})
+
+  return(
+    <div>
+      {clicks.left}
+      <button onClick={handleLeftClick}>{'<- Left'}</button>
+      <button onClick={handleRightClick}>{'Right ->'}</button>
+      {clicks.right}
+    </div>
   )
+
+  
 }
 
 export default App
